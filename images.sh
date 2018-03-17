e29e3b215ed81b96ba56ae91b1c26e90c72148d4
Andressa Valadares <andressarodrial@gmail.com>
2018-03-17 00:57:49 -0300

    Paths fixed

--- images.sh
+++ images.sh
@@ -7,50 +7,50 @@
 
 #!/bin/bash
 
-DIR=$1 #sequence fasta
-OUT=$2      # xml with regions data
+readonly DIR=$1 #sequence fasta
+readonly OUT=$2 # xml with regions data
 
 function utr5 {
-    cd 5UTR/
+    local reg="${DIR}/5UTR"
     echo "${OUT} 5'UTR"
 
-    prinseq-lite.pl -fasta *5UTR.fa -derep 1 -verbose -out_format 1 -out_good ${OUT}_norep 
-    muscle -in ${OUT}_norep.fasta -clwstrict -out ${OUT}.aln    
-    RNAz ${OUT}.aln | head -n 23
-    RNAz ${OUT}.aln > ${OUT}.out
-    RNAalifold -p ${OUT}.aln 
+    #prinseq-lite.pl -fasta *5UTR.fa -derep 1 -verbose -out_format 1 -out_good ${OUT}_norep 
+    muscle -in ${reg}/${OUT}_norep.fasta -clwstrict -out ${reg}/${OUT}.aln    
+    RNAz ${reg}/${OUT}.aln | head -n 23
+    RNAz ${reg}/${OUT}.aln > ${reg}/${OUT}.out
+    RNAalifold -p ${reg}/${OUT}.aln 
 
     pwd 
 
     # run cmount.pl
-    perl ../../../../ViennaRNA-2.4.1/src/Utils/cmount.pl alidot.ps > ${OUT}_cmount.ps
+    perl cmount.pl ${reg}/alidot.ps > ${reg}/${OUT}_cmount.ps
     # run relplot.pl
-    perl ../../../../ViennaRNA-2.4.1/src/Utils/relplot.pl alirna.ps alidot.ps > ${OUT}_relplot.ps
+    perl relplot.pl ${reg}/alirna.ps ${reg}/alidot.ps > ${reg}/${OUT}_relplot.ps
     # run coloraln.pl
-    perl ../../../../ViennaRNA-2.4.1/src/Utils/coloraln.pl -s alirna.ps ${OUT}.aln > ${OUT}_coloraln.ps
+    perl coloraln.pl -s ${reg}/alirna.ps ${reg}/${OUT}.aln > ${reg}/${OUT}_coloraln.ps
 
     cd ..
 }
 
 function utr3 {
     pwd	
-    cd 3UTR/
     for x in {1..3}
     do
         echo "${OUT} 3'UTR - R${x}"
-        cd R${x}/
-        prinseq-lite.pl -fasta ${OUT}_R${x}.fa -derep 1 -verbose -out_format 1 -out_good ${OUT}_R${x}_norep
-        muscle -in ${OUT}_R${x}_norep.fasta -clwstrict -out ${OUT}_R${x}.aln    
-        RNAz ${OUT}_R${x}.aln > ${OUT}_R${x}.out
-        RNAz ${OUT}_R${x}.aln | head -n 23
-        RNAalifold -p ${OUT}_R${x}.aln 
+        local reg="${DIR}/3UTR/R${x}"
+
+        #prinseq-lite.pl -fasta ${OUT}_R${x}.fa -derep 1 -verbose -out_format 1 -out_good ${OUT}_R${x}_norep
+        muscle -in ${reg}/${OUT}_R${x}_norep.fasta -clwstrict -out ${reg}/${OUT}_R${x}.aln    
+        RNAz ${reg}/${OUT}_R${x}.aln > ${reg}/${OUT}_R${x}.out
+        RNAz ${reg}/${OUT}_R${x}.aln | head -n 23
+        RNAalifold -p ${reg}/${OUT}_R${x}.aln 
 
         # run cmount.pl
-        perl ../../../../../ViennaRNA-2.4.1/src/Utils/cmount.pl alidot.ps > ${OUT}_R${x}_cmount.ps
+        perl cmount.pl ${reg}/alidot.ps > ${reg}/${OUT}_R${x}_cmount.ps
         # run relplot.pl
-        perl ../../../../../ViennaRNA-2.4.1/src/Utils/relplot.pl alirna.ps alidot.ps > ${OUT}_R${x}_relplot.ps
+        perl relplot.pl ${reg}/alirna.ps alidot.ps > ${reg}/${OUT}_R${x}_relplot.ps
         # run coloraln.pl
-        perl ../../../../../ViennaRNA-2.4.1/src/Utils/coloraln.pl -s alirna.ps ${OUT}_R${x}.aln > ${OUT}_R${x}_coloraln.ps
+        perl coloraln.pl -s alirna.ps ${reg}/${OUT}_R${x}.aln > ${reg}/${OUT}_R${x}_coloraln.ps
 
         cd ..
     done
@@ -59,7 +59,6 @@ function utr3 {
 
 
 echo "Run prinseq, muscle, RNAalifold and RNAz"
-    cd ${DIR}/
     utr5
     utr3
     exit
